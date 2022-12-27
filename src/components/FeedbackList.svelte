d770a5884d82dbd434e24638a1e81ab3048738e1
marouane11119999 <marouanesoussi144@gmail.com>
2022-12-27 12:02:45 +0100

    simple Svelte Application for reviews

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {FeedbackStore} from '../stores'
+    import {onMount,onDestroy} from 'svelte'
+    import { each} from "svelte/internal";
+    import {fade, scale} from 'svelte/transition'
+    import FeedbackItem from "./FeedbackItem.svelte";
+
+     
+    
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+    <div in:scale out:fade>
+        <FeedbackItem item={fb}/>
+    </div>
+{/each}
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { FeedbackStore } from "../stores";
+    $:count=$FeedbackStore.length
+    $:average=($FeedbackStore.reduce((a,{rating})=>a+rating,0)/$FeedbackStore.length).toFixed(1)
+	
+    
+    
+
+</script>
+
+<div class="feedback-stats">
+    <h4>{count} Reviews</h4>
+    {#if count==0}
+        <h4>Rating Average : There is no Reviews</h4>
+    {:else}
+        <h4>Rating Average : {average}</h4>   
+    {/if}
+</div>
+
+
+<style>
+    .feedback-stats{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
